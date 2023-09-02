54f18bafd00cc0f8d3ec23043011dd4766c75d04
MFurkan-Saglam <81245955+MFurkan-Saglam@users.noreply.github.com>
2023-09-03 02:39:30 +0300

    Delete mhrs_furkan/northwind directory

--- mhrs_furkan/northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/JobsDao.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.northwind.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kodlamaio.northwind.entities.concretes.Jobs;
-
-public interface JobsDao extends JpaRepository<Jobs, Integer> {
-
-}
